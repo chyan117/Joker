7a94ece84c0f5691a1a85b11d385ef5683ce1f41
Hsin-Chien Chang <kingchang33@gmail.com>
2020-08-07 16:56:44 +0800

    r

---
+++ src/test/java/guru/springframework/joker/joker/JokerApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.joker.joker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
